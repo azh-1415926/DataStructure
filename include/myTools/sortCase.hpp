53f8f8e432f6a7fc629abf95b76f9a821fa743f4
azh-1415926 <AzhandHJ@outlook.com>
2024-02-18 11:00:25 +0800

    sortCase(C++) 增加此类，用于自动生成排序用例，并应用到 sort 测试代码中

---
+++ include/myTools/sortCase.hpp
@@ -0,0 +1,97 @@
+#ifndef SORT_CASE_H
+#define SORT_CASE_H
+
+#include <functional>
+#include <simpleDS/vector.hpp>
+
+namespace azh
+{
+    /* T1 为排序示例的数据类型，T2 为生成数据的种子类型，T3为用于生成数据的函数容器类型 */
+    template<class T1,class T2=int,class T3=std::function<T1(T2)>>
+    class sortCase
+    {
+        private:
+            size_t m_Sum;
+            T2 m_LowerLimit;
+            T2 m_UpperLimit;
+            T3 m_FuncOfRand;
+
+        public:
+            explicit sortCase(size_t n,const T2& lowerLimit,const T2& upperLimit)
+                : m_Sum(10), m_LowerLimit(lowerLimit), m_UpperLimit(upperLimit) {  }
+
+            inline void setRandFunction(T3 funcOfRand) { m_FuncOfRand=funcOfRand; }
+            
+            vector<T1> getNoRepeat();
+            vector<T1> getSame(size_t refreshCount);
+
+            static bool verify(vector<T1> vec,std::function<int(const T1&,const T1&)> comparision);
+
+        private:
+            vector<T1> getNoRepeatData();
+    };
+
+    template<class T1,class T2,class T3>
+    vector<T1> sortCase<T1,T2,T3>::getNoRepeat()
+    {
+        if(!m_FuncOfRand)
+            throw "sortCase:not call setRandFunction!";
+        return getNoRepeatData();
+    }
+
+    template<class T1,class T2,class T3>
+    vector<T1> sortCase<T1,T2,T3>::getSame(size_t refreshCount)
+    {
+        if(!m_FuncOfRand)
+            throw "sortCase:not call setRandFunction!";
+        static vector<T> vec;
+        static size_t count=0;
+        if(count<refreshCount)
+        {
+            vec=getNoRepeatData();
+            count++;
+        }
+        return vec;
+    }
+
+    template<class T1,class T2,class T3>
+    vector<T1> sortCase<T1,T2,T3>::getNoRepeatData()
+    {
+        vector<T1> vec;
+        auto seed=m_UpperLimit-m_LowerLimit;
+        if(seed<m_Sum)
+            throw "sortCase:seed is smaller than n!";
+        for(T2 i=m_LowerLimit;i<m_UpperLimit;)
+        {
+            T1 data=m_FuncOfRand(i);
+            bool isVaild=true;
+            for(size_t j=0;j<vec.size();j++)
+            {
+                if(data==vec[j])
+                {
+                    isVaild=false;
+                    break;
+                }
+            }
+            if(isVaild)
+            {
+                vec.push_back(data);
+                i++;
+            }
+        }
+        return vec;
+    }
+
+    template<class T1,class T2,class T3>
+    bool sortCase<T1,T2,T3>::verify(vector<T1> vec,std::function<int(const T1&,const T1&)> comparision)
+    {
+        for(auto i=vec.begin();i!=vec.end()-1;i++)
+        {
+            if(comparision(*i,*(i+1))>0)
+                return false;
+        }
+        return true;
+    }
+}
+
+#endif /* SORT_CASE_H */
--- test/Sorting/main.cpp
+++ test/Sorting/main.cpp
@@ -1,31 +1,41 @@
 #include <myDS/sort.hpp>
-#include <vector>
+#include <myTools/sortCase.hpp>
 #include <algorithm>
+
+#define SHOW_CONTAINER_DATA(container,showFunc) \
+    std::cout<<"Show data from container : "<<#container<<"\n"; \
+    for(auto i=container.begin();i!=container.end();i++) \
+    { \
+        showFunc(*i); \
+        std::cout<<" "; \
+    } \
+    std::cout<<"\n";
 
 int comparisionForInt(const int& v1,const int& v2)
 {
     return v1-v2;
 };
 
+int getRandForInt(int seed)
+{
+    srand(time(NULL));
+    seed+=213;
+    return rand()%seed+10;
+}
+
 void printData(int data)
 {
-    std::cout<<data<<"\n";
+    std::cout<<data;
 }
 
 int main()
 {
-    int vec[]={1,3,2,4,5};
-    std::vector<int> vec1={1,3,2,4,5};
-    std::cout<<"vec\n";
-    std::for_each(vec,vec+sizeof(vec)/sizeof(int),printData);
-    std::cout<<"vec1\n";
-    std::for_each(vec1.begin(),vec1.end(),printData);
-    // azh::Sort<int>::bubbleSort(vec,vec+sizeof(vec)/sizeof(int),comparisionForInt);
-    azh::Sort<int>::heapSort(vec,vec+sizeof(vec)/sizeof(int),comparisionForInt);
-    azh::Sort<int,std::vector<int>::iterator>::bubbleSort(vec1.begin(),vec1.end(),comparisionForInt);
-    std::cout<<"vec\n";
-    std::for_each(vec,vec+sizeof(vec)/sizeof(int),printData);
-    std::cout<<"vec1\n";
-    std::for_each(vec1.begin(),vec1.end(),printData);
+    azh::sortCase<int> sortCase(10,0,10);
+    sortCase.setRandFunction(getRandForInt);
+    azh::vector<int> vec=sortCase.getNoRepeat();
+    SHOW_CONTAINER_DATA(vec,printData);
+    azh::Sort<int,azh::vector<int>::iterator>::bubbleSort(vec.begin(),vec.end(),comparisionForInt);
+    SHOW_CONTAINER_DATA(vec,printData);
+    sortCase.verify(vec,comparisionForInt);
     return 0;
 }
